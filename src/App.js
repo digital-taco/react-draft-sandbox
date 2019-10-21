a5253fad09d73a9d35e9fb2c3c9b0b8e4fdfefb9
Zachary Williams <zlw11063@gmail.com>
2019-10-20 21:14:40 -0600

    Add boolean

--- src/App.js
+++ src/App.js
@@ -3,20 +3,27 @@ import './App.css';
 import './index.css'
 import PropTypes from 'prop-types'
 
-function App({ name, bacon = 'yes', cookies = 1000, noPropTypeHere = 'potato'}) {
+function PropValue({ propName, propValue, propType }) {
+  return (
+    <>
+      <div data-prop-type>{propType}</div>{' '}
+      <div data-prop-name>{propName}:</div>{' '}
+      <div data-prop-value>{propValue}</div>
+    </>
+  )
+}
+
+function App({
+  name = 'Draft',
+  cookies = 1000,
+  noPropTypeHere = 'potato',
+  isOpen,
+}) {
   return (
     <div className="App">
-      <header className="App-header">
-        <div>
-          {name}
-        </div>
-        <div>
-          {bacon}
-        </div>
-        <div>
-          {cookies + 100}
-        </div>
-      </header>
+      <PropValue propType="string" propName="name" propValue={name} />
+      <PropValue propType="number" propName="cookies" propValue={cookies} />
+      <PropValue propType="boolean" propName="isOpen" propValue={isOpen ? 'true' : 'false'} />
     </div>
   );
 }
@@ -27,4 +34,9 @@ App.propTypes = {
   name: PropTypes.string,
   bacon: PropTypes.string,
   cookies: PropTypes.number,
+  isOpen: PropTypes.bool,
+  // cookies: PropTypes.object,
+  // cookies: PropTypes.func,
+  // cookies: PropTypes.array,
+  // cookies: PropTypes.arrayOf,
 }

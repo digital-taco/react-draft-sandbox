10322ba293561fc9a5ff416236e6973fc2ff652b
Zachary Williams <zlw11063@gmail.com>
2019-12-10 08:31:19 -0700

    Updates

--- runtime~draft-main.js
+++ runtime~draft-main.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "57606e026d50566a1648";
+/******/ 	var hotCurrentHash = "90e85a0c57e7377003b9";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
@@ -843,7 +843,7 @@
 /******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
 /******/
 /******/ 	// __webpack_public_path__
-/******/ 	__webpack_require__.p = "/";
+/******/ 	__webpack_require__.p = "/META-react-draft-sandbox/";
 /******/
 /******/ 	// __webpack_hash__
 /******/ 	__webpack_require__.h = function() { return hotCurrentHash; };

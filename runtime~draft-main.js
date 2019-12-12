d25a7b184c32cef152ac443c740c3bf7d6c2f487
Zachary Williams <zlw11063@gmail.com>
2019-12-12 07:37:33 -0700

    Updates

--- runtime~draft-main.js
+++ runtime~draft-main.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "3347aba666eeb6283704";
+/******/ 	var hotCurrentHash = "b984bbeac7540470cd51";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;

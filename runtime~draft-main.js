9cfadbf42ff57b54c7d4aa8dcfb57fca0484d9b9
Kyle West <kyle@familysearch.org>
2019-12-18 09:05:02 -0700

    Updates

--- runtime~draft-main.js
+++ runtime~draft-main.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "8c0946abe4baa37fc8b5";
+/******/ 	var hotCurrentHash = "0a200921fcaacdd60a71";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
